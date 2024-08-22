f209e95d065ceca4558704ea59b05e6ebe6679cd
Lokesh <lokesh.mdpd123@gmail.com>
2024-08-22 10:37:09 +0530

    error-handler.js

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -4,7 +4,27 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   if (err instanceof CustomAPIError) {
     return res.status(err.statusCode).json({ msg: err.message })
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+    // const customError = {
+    //     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    //     message: err.message || 'Something went wrong',
+    // }
+    
+    // if(err.name == 'CastError'){
+    //     customError.message = `There is an error in JobId: ${err.value}`
+    // }
+    // if(err.name === 'ValidationError'){
+    //     customError.message = Object.values(err.errors)
+    //     .map((item) => item.message)
+    //     .join(',')
+    //     customError.statusCode = 400
+    // }
+    // if(err.code && err.code === 11000){
+    //     customError.message = `Duplicate value entered for ${err.errorResponse.keyValue.email}  field, choose another value`,
+    //     customError.statusCode = 400
+    // }
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
+    // return res.status(customError.statusCode).json({message: customError.message})
+  
 }
 
 module.exports = errorHandlerMiddleware
